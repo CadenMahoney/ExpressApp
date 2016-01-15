0135de0c3a2b1e19f6776a523942b0827b070994
CadenMahoney <cadendmahoney@gmail.com>
2016-01-15 16:00:14 -0700

    Moved project files to the correct folder

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var app = express.Router();
-
-app.get('/', function (req, res) {
-  res.render('index', {title: 'Hello', message: 'Hi there!'});
-});
-
-app.get('/books', function (req, res) {
-  res.send('Hello Books!');
-});
-
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'caden'
+  password : 'caden',
+  database : 'nalcs'
 });
 
-connection.connect();
+connection.connect(function(err){
+  if(!err) {
+    console.log("Database is connected ... \n\n");
+  } else {
+    console.log("Error connecting database ... \n\n");
+  }
+});
 
-  console.log('connected as id ' + connection.threadId);
-
-  connection.query('SELECT firstName AS fn FROM player', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].fn);
+app.get("/",function(req,res){
+  connection.query('SELECT * FROM player', function(err, rows, fields) {
+    connection.end();
+    if (!err)
+      res.send(rows);
+    else
+      console.log('Error while performing Query.');
   });
+});
 
-connection.end();
 
 
 module.exports = app;
